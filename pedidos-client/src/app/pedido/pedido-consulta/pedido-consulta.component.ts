2ed8839f1cf441eb936af5cca5d4921a9da41af2
Erick Henrique <erickhenriquemota@gmail.com>
2019-10-11 01:05:15 -0300

    Cria componente pedido-consulta

---
+++ pedidos-client/src/app/pedido/pedido-consulta/pedido-consulta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-consulta',
+  templateUrl: './pedido-consulta.component.html',
+  styleUrls: ['./pedido-consulta.component.css']
+})
+export class PedidoConsultaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
